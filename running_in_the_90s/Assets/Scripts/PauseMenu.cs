3692b1a55c3fb0144437fb93258306b77a01add9
Lasillya <divna1999@gmail.com>
2022-04-29 09:16:04 +0200

    Adding quit button in pause menu
    Adding quit button in pause menu

--- running_in_the_90s/Assets/Scripts/PauseMenu.cs
+++ running_in_the_90s/Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -34,5 +35,11 @@ public class PauseMenu : MonoBehaviour
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
         IsPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
     }
 }
