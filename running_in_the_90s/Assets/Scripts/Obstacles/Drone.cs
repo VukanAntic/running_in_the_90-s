0647597dd70a781b7b313e5e44ad6dcd43d1d90b
mi18145 <mi18145@alas.matf.bg.ac.rs>
2022-05-13 23:38:36 +0200

    Add: Made Drones and UFOs move and change color
    Added scripts to make Drones and UFOs bob up and down as well as change colors periodically

--- running_in_the_90s/Assets/Scripts/Obstacles/Drone.cs
+++ running_in_the_90s/Assets/Scripts/Obstacles/Drone.cs
@@ -5,17 +5,50 @@ using UnityEngine;
 public class Drone : MonoBehaviour
 {
     [SerializeField] private List<Sprite> DroneSprites;
+    private List<Sprite> Chosen;
+
+    private float ColorChangeTime;
+    private int CurrentChosen;
+
+    private float Speed;
+    private int Direction;
+    private float MaxMove;
+    private float Move;
 
     // Start is called before the first frame update
     void Start()
     {
+
+        Chosen = new List<Sprite>();
         int droneNumber = Random.Range(0, DroneSprites.Count);
-        this.GetComponent<SpriteRenderer>().sprite = DroneSprites[droneNumber];
+        Chosen.Add(DroneSprites[droneNumber]);
+        Chosen.Add(DroneSprites[(droneNumber + Random.Range(1, DroneSprites.Count)) % DroneSprites.Count]);
+        this.GetComponent<SpriteRenderer>().sprite = Chosen[0];
+
+        Speed = 0.02f * Random.Range(2,10);
+        MaxMove = 0.2f;
+        Move = 0f;
+        Direction = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float currMove = Speed * Time.deltaTime;
+        transform.position += new Vector3(0,Direction * currMove,0);
+        Move += currMove;
+        if(Move >= MaxMove)
+        {
+            Direction *= -1;
+            Move = 0;
+        }
+
+        ColorChangeTime += Time.deltaTime;
+        if (ColorChangeTime >= 0.5)
+        {
+            ColorChangeTime = 0;
+            CurrentChosen = (CurrentChosen + 1) % 2;
+            this.GetComponent<SpriteRenderer>().sprite = Chosen[CurrentChosen];
+        }
     }
 }
